89996e332b002ac964c21c3d2ab196b5728e149b
Giles Dring <giles@dringtech.com>
2024-06-13 15:53:35 +0100

    Setup and apply formatter

--- _config.ts
+++ _config.ts
@@ -10,7 +10,7 @@ import sitemap from "lume/plugins/sitemap.ts";
 import svgo from "lume/plugins/svgo.ts";
 
 const site = lume({
-  src: './src',
+  src: "./src",
 });
 
 site.use(base_path());
@@ -38,27 +38,34 @@ site.use(feed({
 
 // Copy typefaces
 [
-  'DenimINK-Bold-WY4SU2TF.woff2',
-  'DenimINK-Light-J3BAZLHK.woff2',
-  'DenimINK-Regular-YY7M2VJL.woff2',
-  'DenimINK-SemiBold-6EI7BXBD.woff2',
-].forEach(f => site.remoteFile(`assets/fonts/${f}`, `vendor/bd25/${f}`));
+  "DenimINK-Bold-WY4SU2TF.woff2",
+  "DenimINK-Light-J3BAZLHK.woff2",
+  "DenimINK-Regular-YY7M2VJL.woff2",
+  "DenimINK-SemiBold-6EI7BXBD.woff2",
+].forEach((f) => site.remoteFile(`assets/fonts/${f}`, `vendor/bd25/${f}`));
 
-site.copy('assets/fonts/');
+site.copy("assets/fonts/");
 
 // Copy Reveal code
 [
-  'dist/reveal.js',
-  'dist/reveal.css',
-].forEach(f => site.remoteFile(`assets/vendor/reveal/${f}`, `vendor/reveal.js-master/${f}`));
+  "dist/reveal.js",
+  "dist/reveal.css",
+].forEach((f) =>
+  site.remoteFile(`assets/vendor/reveal/${f}`, `vendor/reveal.js-master/${f}`)
+);
 
-site.copy('assets/vendor/');
+site.copy("assets/vendor/");
 
 [
   "mixins.scss",
   "exposer.scss",
   "settings.scss",
   "theme.scss",
-].forEach(c => site.remoteFile(`_includes/scss/${c}`, `vendor/reveal.js-master/css/theme/template/${c}`))
+].forEach((c) =>
+  site.remoteFile(
+    `_includes/scss/${c}`,
+    `vendor/reveal.js-master/css/theme/template/${c}`,
+  )
+);
 
 export default site;
