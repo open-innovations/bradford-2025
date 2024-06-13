24e43bc006ab5303cc22caf59c7acca16883bb80
Giles Dring <giles@dringtech.com>
2024-06-13 12:41:56 +0100

    Set up basic site

---
+++ _config.ts
@@ -0,0 +1,31 @@
+import lume from "lume/mod.ts";
+import base_path from "lume/plugins/base_path.ts";
+import date from "lume/plugins/date.ts";
+import esbuild from "lume/plugins/esbuild.ts";
+import feed from "lume/plugins/feed.ts";
+import postcss from "lume/plugins/postcss.ts";
+import sitemap from "lume/plugins/sitemap.ts";
+
+const site = lume({
+  src: './src',
+});
+
+site.use(base_path());
+site.use(date());
+site.use(esbuild());
+site.use(postcss());
+site.use(sitemap());
+site.use(feed({
+  output: ["/site.rss", "/site.json"],
+  query: "type=metric",
+  info: {
+    title: "=site.title",
+    description: "=site.description",
+  },
+  items: {
+    title: "=title",
+    description: "=excerpt",
+  },
+}));
+
+export default site;
