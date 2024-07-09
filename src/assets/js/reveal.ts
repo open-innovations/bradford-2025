b4bfe372b442aa995153c50579473b49e87e4fb3
Giles Dring <giles@dringtech.com>
2024-07-09 13:12:22 +0100

    Convert to regular rejeal presentation

--- src/assets/js/reveal.ts
+++ src/assets/js/reveal.ts
@@ -2,9 +2,9 @@ import Reveal from "npm:reveal.js";
 
 const deck = new Reveal({
   // Activate the scroll view
-  view: "scroll",
+  // view: "scroll",
 
   // Force the scrollbar to remain visible
-  scrollProgress: true,
+  // scrollProgress: true,
 });
 deck.initialize();
