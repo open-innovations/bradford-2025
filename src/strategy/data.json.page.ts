9c51097974dd00ea21e8a4dc1a8618722ecdf30a
Giles Dring <giles@dringtech.com>
2024-07-10 23:15:21 +0100

    Flatten topics and outputs

--- src/strategy/data.json.page.ts
+++ src/strategy/data.json.page.ts
@@ -1,23 +1,19 @@
 export const url = "/strategy/data.json";
 export const layout = "templates/none.vto"
 
-export default function ({ output, source }) {
-  const topics = output.map(({ id }) => ({ id, r: 10 }))
-  const indicator = output.map(({ indicator }) => indicator).flat().filter((x, i, a) => a.map(x => x.id).indexOf(x.id) == i);
+const firstMatch = <T>(x: T, i: number, a: T[]) => a.map(x => x.id).indexOf(x.id) == i
+
+export default function ({ topic, output, source }) {
 
   const nodes = [
-    ...topics.map(({ id }) => ({ id, r: 5, type: "topic" })),
-    ...indicator.map(({ id }) => ({ id, r: 5, type: "output" })),
+    ...topic.map(({ id }) => ({ id, r: 5, type: "topic" })),
+    ...output.map(({ id }) => ({ id, r: 5, type: "output" })),
     ...source.map(({ id }) => ({ id, r: 5, type: "system" })),
   ]
   
   const links = [
-    ...output.map((s) => s.indicator
-      .map(t => ({
-        source: s.id, target: t.id
-      }))
-    ).flat(),
-    ...indicator.map((s) => (s.source || [])
+    ...output.map((s) => ({ source: s.topic, target: s.id })),
+    ...output.map((s) => (s.source || [])
       .map(t => ({
         source: s.id, target: t
       }))
