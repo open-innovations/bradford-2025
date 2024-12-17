b1e0ac0814025df501d5be97751edaae2d1c5abf
Giles Dring <giles@dringtech.com>
2024-12-17 23:22:32 +0000

    Add initial WIP on huq data

--- pipelines/utils/paths.py
+++ pipelines/utils/paths.py
@@ -3,5 +3,8 @@ from pathlib import Path
 ROOT = (Path(__file__).parent / '../..').resolve()
 
 DATA = ROOT / 'data/published'
+PUBLISHED = ROOT / 'data/published'
+
+HUQ = ROOT / 'data/huq'
 
 SITE = ROOT / 'src'
