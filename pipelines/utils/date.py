507c9ca2effdf1de81d6d162e228c8989a02c206
Giles Dring <giles@dringtech.com>
2025-01-23 08:56:33 +0000

    Refine breakdowns by type
    Updates #3

---
+++ pipelines/utils/date.py
@@ -0,0 +1,10 @@
+import datetime
+import pandas as pd
+
+
+def date_to_week(date) -> datetime.datetime:
+    return pd.Period(date, 'W-SUN').to_timestamp(how='End').floor('D').to_pydatetime()
+
+
+def date_to_month(date) -> datetime.datetime:
+    return pd.Period(date, 'M').to_timestamp(how='Start').floor('D').to_pydatetime()
