155228bcaa3f6f303284d77896c80126f4030bab
Giles Dring <giles@dringtech.com>
2025-02-05 12:24:03 +0000

    Make catalogue draft

--- src/catalogue/index.page.ts
+++ src/catalogue/index.page.ts
@@ -16,7 +16,8 @@ export default function *({ metadata, url: baseUrl, search }) {
             name,
             key,
             title,
-            schema
+            schema,
+            draft: true,
         }
     }
 }
