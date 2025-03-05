0af88f61ce7ca0296948b37f1e5b6d59329f7bdf
Giles Dring <giles@dringtech.com>
2025-03-05 14:55:27 +0000

    Hide elements from CMS

--- _cms.ts
+++ _cms.ts
@@ -4,32 +4,32 @@ const cms = lumeCMS();
 
 // Configuration here
 
-cms.collection({
-    name: "pages",
-    description: "Individual pages managed via CMS",
-    store: "src:pages/*.md",
-    fields: [
-        { name: "title", type: "text", attributes: { required: true } },
-        { name: "description", type: "markdown" },
-        { name: "draft", type: "checkbox" },
-        { name: "url", label: "URL", type: "text" },
-        { name: "content", type: "markdown" },
-    ],
-    nameField: "title"
-});
+// cms.collection({
+//     name: "pages",
+//     description: "Individual pages managed via CMS",
+//     store: "src:pages/*.md",
+//     fields: [
+//         { name: "title", type: "text", attributes: { required: true } },
+//         { name: "description", type: "markdown" },
+//         { name: "draft", type: "checkbox" },
+//         { name: "url", label: "URL", type: "text" },
+//         { name: "content", type: "markdown" },
+//     ],
+//     nameField: "title"
+// });
 
 cms.upload("assets", "src:assets/");
 
-cms.collection({
-    name: "blocks",
-    description: "Content blocks referenced in the site",
-    store: "src:_includes/cms/block/*.md",
-    fields: [
-        { name: "reference", type: "text", view: "admin" },
-        { name: "content", type: "markdown" },
-    ],
-    nameField: "reference"
-});
+// cms.collection({
+//     name: "blocks",
+//     description: "Content blocks referenced in the site",
+//     store: "src:_includes/cms/block/*.md",
+//     fields: [
+//         { name: "reference", type: "text", view: "admin" },
+//         { name: "content", type: "markdown" },
+//     ],
+//     nameField: "reference"
+// });
 
 cms.collection({
     name: "Dashboard metrics",
@@ -41,5 +41,8 @@ cms.collection({
         { name: "description", type: "markdown" }
     ],
     documentName: "{title}.yml",
+    // create: false,
+    delete: false,
 })
+
 export default cms;
