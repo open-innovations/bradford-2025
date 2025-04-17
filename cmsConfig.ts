ce58ce3d8a53aed93c72eb8bd53ca08207e56d3b
luke-strange <92686634+luke-strange@users.noreply.github.com>
2025-04-17 10:51:31 +0100

    Update table_descriptions description

--- cmsConfig.ts
+++ cmsConfig.ts
@@ -4,7 +4,7 @@ const dashboardFields = [
     { name: "flash", type: "text", description: 'The text displayed in the the ribbon across the top-right of the panel' },
     { name: "upTo", type: "date", description: 'The date shown on the dashboard panel' },
     { name: "notes", description: "Internal notes, not for public display", type: "markdown", upload: false },
-    { name: "table_descriptions", description: "The table will contain all events. If you'd like to add a description to an event, you can do so below. The event title must match the name of the event exactly (be wary of trailing spaces, hyphens, capital letters and special characters.", type: "object-list", fields: ["Event title: text", "description: text"]}
+    { name: "table_descriptions", description: "Tables in the 'i' popover will show all sources of data that make up the total. If you'd like to add a description to a row of the table, you can do so below. The title must match the name of the row exactly (be wary of trailing spaces, hyphens, capital letters and special characters).", type: "object-list", fields: ["Event title: text", "description: text"]}
     ];
 
 export default dashboardFields;
