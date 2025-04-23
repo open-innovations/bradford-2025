bafdc77175795c6e661dbb201fc836df9b4e752c
luke-strange <92686634+luke-strange@users.noreply.github.com>
2025-04-23 15:39:39 +0100

    Set-up CMS with GitHub

--- _cms.ts
+++ _cms.ts
@@ -1,30 +1,40 @@
 import lumeCMS from "lume/cms/mod.ts";
 import dashboardFields from "./cmsConfig.ts";
+import GitHub from "lume/cms/storage/github.ts";
+import { Octokit } from "npm:octokit";
 
-const cms = lumeCMS();
+// Load users from environment variables prefixed with CMS_USER_
+const username = Deno.env.get("USERNAME")!;
+const password = Deno.env.get("PASSWORD")!;
+const token = Deno.env.get("GITHUB_TOKEN")!;
+
+const cms = lumeCMS({
+    auth: {
+        method: "basic",
+        users: {
+            [username]: password,
+          },
+    },
+});
 
 // Configuration here
 
-// cms.collection({
-//     name: "pages",
-//     description: "Individual pages managed via CMS",
-//     store: "src:pages/*.md",
-//     fields: [
-//         { name: "title", type: "text", attributes: { required: true } },
-//         { name: "description", type: "markdown" },
-//         { name: "draft", type: "checkbox" },
-//         { name: "url", label: "URL", type: "text" },
-//         { name: "content", type: "markdown" },
-//     ],
-//     nameField: "title"
-// });
+// Configure the CMS to use GitHub as the storage method
+cms.storage(
+    "gh",
+    new GitHub({
+      client: new Octokit({ auth: Deno.env.get("GITHUB_TOKEN") }),
+      owner: "open-innovations",
+      repo: "bradford-2025",
+    }),
+  );
 
-cms.upload("assets", "src:assets/");
+cms.upload("assets", "gh:src/assets/");
 
 // cms.collection({
 //     name: "blocks",
 //     description: "Content blocks referenced in the site",
-//     store: "src:_includes/cms/block/*.md",
+//     store: "gh:src/_includes/cms/block/*.md",
 //     fields: [
 //         { name: "reference", type: "text", view: "admin" },
 //         { name: "content", type: "markdown" },
@@ -34,7 +44,7 @@ cms.upload("assets", "src:assets/");
 
 cms.collection({
     name: "2025 dashboard",
-    store: "src:insights/dashboard/_data/metrics/*.yml",
+    store: "gh:src/insights/dashboard/_data/metrics/*.yml",
     fields: dashboardFields,
     documentName: "{title}.yml",
     // create: false,
@@ -43,7 +53,7 @@ cms.collection({
 
 cms.collection({
     name: "Janunary to March 2025 dashboard",
-    store: "src:insights/dashboard/q1/_data/metrics/*.yml",
+    store: "gh:src/insights/dashboard/q1/_data/metrics/*.yml",
     fields: dashboardFields,
     documentName: "{title}.yml",
     // create: false,
@@ -52,7 +62,7 @@ cms.collection({
 
 cms.collection({
     name: "April to June 2025 dashboard",
-    store: "src:insights/dashboard/q1/_data/metrics/*.yml",
+    store: "gh:src/insights/dashboard/q1/_data/metrics/*.yml",
     fields: dashboardFields,
     documentName: "{title}.yml",
     // create: false,
@@ -61,7 +71,7 @@ cms.collection({
 
 cms.collection({
     name: "Glossary",
-    store: "src:glossary/term/*.md",
+    store: "gh:src/glossary/term/*.md",
     fields: [ 
         { name: "title", type: "text", attributes: { required: true } },
         { name: "description", type: "markdown" },
