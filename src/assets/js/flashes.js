c66a95db09bf5e9403c203db36f7121bddf9f714
slowe <299787+slowe@users.noreply.github.com>
2025-06-18 15:59:44 +0100

    Fix clash with tooltips

--- src/assets/js/flashes.js
+++ src/assets/js/flashes.js
@@ -7,7 +7,7 @@
 	};
 	
 	var styles = document.createElement('style');
-	styles.innerHTML = '.tooltip {display:none;z-index:10000;color: var(--color,var(--color-white)); filter:drop-shadow(0px 1px 1px rgba(0,0,0,0.7));text-align:left; max-width:min(250px,100%);position:absolute;transform:translate3d(0,-100%,0);top:0;right:0;}.tooltip .inner { padding: 1em; background: var(--bgcolor,var(--color-black)); position: relative; transform: none; border: 0px; }.tooltip .arrow { position: absolute; width: 0px; height: 0px; border-top-width: 0.5em; border-top-style: solid; border-right-width: 0.5em; border-right-style: solid; border-left: 0.5em solid transparent; border-image: none; left: calc(100% - 2em); top: calc(100% - 1px); transform: translate3d(calc(-50% - 0px), 0px, 0px); border-right-color: transparent; border-top-color: var(--bgcolor,var(--color-black)); }';
+	styles.innerHTML = '.flash-tooltip {display:none;z-index:10000;color: var(--color,var(--color-white)); filter:drop-shadow(0px 1px 1px rgba(0,0,0,0.7));text-align:left; max-width:min(250px,100%);position:absolute;transform:translate3d(0,-100%,0);top:0;right:0;}.flash-tooltip .inner { padding: 1em; background: var(--bgcolor,var(--color-black)); position: relative; transform: none; border: 0px; }.flash-tooltip .arrow { position: absolute; width: 0px; height: 0px; border-top-width: 0.5em; border-top-style: solid; border-right-width: 0.5em; border-right-style: solid; border-left: 0.5em solid transparent; border-image: none; left: calc(100% - 2em); top: calc(100% - 1px); transform: translate3d(calc(-50% - 0px), 0px, 0px); border-right-color: transparent; border-top-color: var(--bgcolor,var(--color-black)); }';
 	document.head.prepend(styles);
 	
 	function addTooltip(el){
@@ -16,7 +16,7 @@
 		var txt = flash.getAttribute('title');
 		if(txt){
 			var vis = false;
-			tt.classList.add('tooltip');
+			tt.classList.add('flash-tooltip');
 			tt.innerHTML = '<div class="inner">' + txt + '</div><div class="arrow"></div>'
 			el.parentNode.append(tt);
 			el.parentNode.style.position = "relative";
