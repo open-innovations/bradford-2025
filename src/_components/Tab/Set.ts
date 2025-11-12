d50d759b91a6e7448eafa73f35aca04ac9ff7a48
slowe <299787+slowe@users.noreply.github.com>
2025-11-12 11:40:03 +0000

    Change content placement in tabbed panels

--- src/_components/Tab/Set.ts
+++ src/_components/Tab/Set.ts
@@ -2,29 +2,24 @@ import { asyncWrapProviders } from "node:async_hooks";
 import { DOMParser } from '../../../deps/dom.ts';
 
 interface TabSetOptions {
-  content: string;
-  wrapperClass?: string;
-  selected: number;
+	content: string;
+	wrapperClass?: string;
+	selected: number;
 }
 
 export default function ({ content, wrapperClass, selected, classes }: TabSetOptions) {
-  const fragment = new DOMParser().parseFromString(content, 'text/html');
-  const panels = Array.from(fragment.querySelectorAll('[data-tab-label]'));
+	const fragment = new DOMParser().parseFromString(content, 'text/html');
+	const panels = Array.from(fragment.querySelectorAll('[data-tab-label]'));
 
-  const tabList = document.createElement('ul');
-  tabList.setAttribute('role','tablist');
-  tabList.setAttribute('aria-label','Visualisations');
+	const tabList = document.createElement('ul');
+	tabList.setAttribute('role','tablist');
+	tabList.setAttribute('aria-label','Visualisations');
 
-  for (const [idx, panel] of panels.entries()) {
-    if (!panel.id) panel.setAttribute('id', `section${idx + 1}`);
-    const tab = document.createElement('li');
-	tab.setAttribute('role','presentation');
-    tab.innerHTML = `<a href="#${ panel.id }" role="tab" data-tab="${ idx }">${ panel.dataset.tabLabel }</a>`;
-	tabList.append(tab);
-  }
-  return `<tabbed data-dependencies="/assets/js/tabbed.js"` + (selected ? ' data-default="'+selected+'"' : '') + (classes ? ' class="'+classes+'"' : '') +`>${
-    tabList.outerHTML
-  }<div ${ wrapperClass ? ` class="${ wrapperClass }"`: "" }>${
-    panels.map(p => p.outerHTML).join('')
-  }</div></tabbed>`;
+	for (const [idx, panel] of panels.entries()) {
+		const tab = document.createElement('li');
+		tab.setAttribute('role','presentation');
+		tab.innerHTML = `<a href="#${ panel.id }" role="tab" data-tab="${ idx }">${ panel.dataset.tabLabel }</a>`;
+		tabList.append(tab);
+	}
+	return '<tabbed data-dependencies="/assets/js/tabbed.js"' + (selected ? ' data-default="' + selected + '"' : '') + (classes ? ' class="' + classes + '"' : '') + '>' + tabList.outerHTML + '<div ' + (wrapperClass ? ' class="' + wrapperClass + '"': "") + '>' + content + '</div></tabbed>';
 }
