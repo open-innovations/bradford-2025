83776224f020acaa71b2c283a6b0be8aeb4b8d3a
Giles Dring <giles@dringtech.com>
2025-12-07 13:47:20 +0000

    Split audience and events calculated metrics

--- src/_data/calculated/programme/audience.js
+++ src/_data/calculated/programme/audience.js
@@ -8,41 +8,18 @@
  * Pipelines in this repo have become somewhat fragile, presenting conflicting and
  * confusing data.
  */
-import { loadCsv } from '../../../lib/data-helpers.js';
-
-function cleanGraphData(row) {
-  return { Date: row.month.slice(0, -3), value: row.value }
-}
+import { loadCsv, cleanGraphData } from 'lib/data-helpers.js';
 
 // TODO this might change name
 const totals = await loadCsv("src/_data/published/programme/total.csv");
 
 // Calculate
-const open_events = totals.find((r) =>
-  r.event_type == "Open" && r.variable == "events"
-);
-const all_events = totals.find((r) =>
-  r.event_type == "ALL" && r.variable == "events"
-);
-const all_audience = totals.find((r) =>
+const allAudience = totals.find((r) =>
   r.event_type == "ALL" && r.variable == "audience"
 );
 
 // MONTHLY DATA
 const byMonth = await loadCsv("src/_data/published/programme/by_month.csv");
-const allEventsByMonth = byMonth.filter((r) =>
-  r.month.match(/^2025-/) &&
-  r.event_type == "ALL" &&
-  r.variable_group == "ALL" &&
-  r.variable == "events"
-).map(cleanGraphData);
-
-const openEventsByMonth = byMonth.filter((r) =>
-  r.event_type == "Open" &&
-  r.month.match(/^2025-/) &&
-  r.variable_group == "ALL" &&
-  r.variable == "events"
-).map(cleanGraphData);
 
 const allAudienceByMonth = byMonth.filter((r) =>
   r.month.match(/^2025-/) &&
@@ -54,8 +31,8 @@ const allAudienceByMonth = byMonth.filter((r) =>
 // CATEGORY DATA
 const byCategory = await loadCsv("src/_data/published/programme/by_category.csv");
 
-const digital_audience = byCategory.find(r => r.month == 'ALL' && r.event_type == 'ALL' && r.variable == 'audience' && r.evaluation_category == 'Digital');
-const non_digital_audience = byCategory.filter(r => r.month == 'ALL' && r.event_type == 'ALL' && r.variable == 'audience' && r.evaluation_category != 'Digital').reduce((a, c) => ({ value: a.value + c.value}), { value: 0 });
+const digitalAudience = byCategory.find(r => r.month == 'ALL' && r.event_type == 'ALL' && r.variable == 'audience' && r.evaluation_category == 'Digital');
+const nonDigitalAudience = byCategory.filter(r => r.month == 'ALL' && r.event_type == 'ALL' && r.variable == 'audience' && r.evaluation_category != 'Digital').reduce((a, c) => ({ value: a.value + c.value}), { value: 0 });
 
 const digitalAudienceByMonth = byCategory.filter(r => r.evaluation_category == 'Digital' && r.month.match(/^2025/) && r.event_type == 'ALL' && r.variable_group == 'ALL' && r.variable == 'audience').map(cleanGraphData);
 const nonDigitalAudienceByMonth = allAudienceByMonth.map(({ Date, value }, i) => ({ Date, value: value - (digitalAudienceByMonth.find(r => r.Date === Date)?.value || 0) }))
@@ -63,21 +40,16 @@ const nonDigitalAudienceByMonth = allAudienceByMonth.map(({ Date, value }, i) =>
 // This exposes the variables to the build. They will be available as `calculated.programme.x`
 // (where `x` is the name in the export object)
 export default {
-  events_data: totals,
-  all_events,
-  open_events,
-  all_audience,
+  all: allAudience,
   categories: {
-    digital_audience,
-    non_digital_audience,
+    digital: digitalAudience,
+    nonDigital: nonDigitalAudience,
   },
   graph: {
-    all_events: allEventsByMonth,
-    open_events: openEventsByMonth,
-    all_audience: allAudienceByMonth,
+    all: allAudienceByMonth,
     categories: {
-      digital_audience: digitalAudienceByMonth,
-      non_digital_audience: nonDigitalAudienceByMonth,
+      digital: digitalAudienceByMonth,
+      nonDigital: nonDigitalAudienceByMonth,
     }
   },
 };
---
+++ src/_data/calculated/programme/events.js
@@ -0,0 +1,48 @@
+/**
+ * This module creates data from 'published' data which represents the agreed
+ * figures key metrics for Bradford 2025.
+ *
+ * This data would have been previously created via pipelines, but this will simplify
+ * the process of revising the content.
+ *
+ * Pipelines in this repo have become somewhat fragile, presenting conflicting and
+ * confusing data.
+ */
+import { loadCsv, cleanGraphData } from "lib/data-helpers.js";
+
+// Load total figures
+const totals = await loadCsv("src/_data/published/programme/total.csv");
+
+const openEvents = totals.find((r) =>
+  r.event_type == "Open" && r.variable == "events"
+);
+const allEvents = totals.find((r) =>
+  r.event_type == "ALL" && r.variable == "events"
+);
+
+// MONTHLY DATA
+const byMonth = await loadCsv("src/_data/published/programme/by_month.csv");
+const allEventsByMonth = byMonth.filter((r) =>
+  r.month.match(/^2025-/) &&
+  r.event_type == "ALL" &&
+  r.variable_group == "ALL" &&
+  r.variable == "events"
+).map(cleanGraphData);
+
+const openEventsByMonth = byMonth.filter((r) =>
+  r.event_type == "Open" &&
+  r.month.match(/^2025-/) &&
+  r.variable_group == "ALL" &&
+  r.variable == "events"
+).map(cleanGraphData);
+
+// This exposes the variables to the build. They will be available as `calculated.programme.events.x`
+// (where `x` is the name in the export object)
+export default {
+  all: allEvents,
+  open: openEvents,
+  graph: {
+    all: allEventsByMonth,
+    open: openEventsByMonth,
+  },
+};
