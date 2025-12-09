b0fe21eac58c877bd7d3ca065389cdef1833f4ac
slowe <299787+slowe@users.noreply.github.com>
2025-12-09 18:22:40 +0000

    Update table colours

--- lib/filters.ts
+++ lib/filters.ts
@@ -1,3 +1,6 @@
+import { ColourScale, defaultColourScale } from 'oi_lume_viz/lib/colour/colour-scale.ts';
+import { contrastColour } from 'oi_lume_viz/lib/colour/contrast.ts';
+
 const wafflify = (sourceData, key = 'key', value = 'value', options = {}) => {
     const {
         minValue,
@@ -29,10 +32,17 @@ export const capitalise = (s: string) => s.charAt(0).toLocaleUpperCase() + s.sli
 
 export const listify = (l: string[], classes = "") => l.map(e => `<li class="${ classes }">${ e }</li>`).join('');
 
+export const colourScale = (p1: number, scale: string, min: number, max: number) => {
+	let cs = ColourScale(scale);
+	return cs((parseFloat(p1)-min)/(max-min));
+};
+
 export default function (site) {
     site.filter('wafflify', wafflify);
     site.filter('capitalise', capitalise);
     site.filter('listify', listify);
+    site.filter('colourScale', colourScale);
+    site.filter('contrastColour', contrastColour);
     return site;
 }
 
