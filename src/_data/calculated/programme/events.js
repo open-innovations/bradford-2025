open-innovations/bradford-2025

commit f0476aa535abe0504b6d645ad2fb76e314977658

More re-wiring with "open" data

src/_data/calculated/programme/events.js
@@ -11,29 +11,32 @@
 import { loadCsv, cleanGraphData, minMaxDates, expandToRange } from "lib/data-helpers.js";
 
 // Load total figures
-const totals = await loadCsv("src/_data/published/programme/total.csv");
+const totals = (await loadCsv("src/_data/published/programme/total.csv")).filter(r => r.variable == 'events');
 
 const openEvents = totals.find((r) =>
-  r.aggregation == 'BY_EVENT_TYPE' && r.event_type == "Open" && r.variable == "events"
+  r.aggregation == 'BY_EVENT_TYPE' && r.event_type == "Open"
 );
 const allEvents = totals.find((r) =>
-  r.aggregation == 'TOTAL' && r.event_type == "ALL" && r.variable == "events"
+  r.aggregation == 'TOTAL' && r.event_type == "ALL"
 );
 
 // MONTHLY DATA
 const byMonth = await loadCsv("src/_data/published/programme/by_month.csv");
 const allEventsByMonth = byMonth.filter((r) =>
-  r.aggregation == 'BY_MONTH' &&
-  r.variable == "events"
+  r.aggregation == 'BY_MONTH'
 ).map(cleanGraphData);
 
 const openEventsByMonth = byMonth.filter((r) =>
   r.aggregation == 'BY_MONTH_BY_EVENT_TYPE' &&
-  r.event_type == "Open" &&
-  r.variable == "events"
+  r.event_type == "Open"
 ).map(cleanGraphData);
 
 const dates = minMaxDates(allEventsByMonth,'Date');
+
+// By category
+// NOTE this is all events, not just Open events.
+const all_by_category = totals.filter(r => r.aggregation == 'BY_EVENT_TYPE').reduce((a, c) => ({ ...a, [c.evaluation_category]: c.value }), {})
+const open_by_category = totals.filter(r => r.aggregation == 'BY_EVENT_TYPE_EVAL_CAT' && r.event_type == 'Open').reduce((a, c) => ({ ...a, [c.evaluation_category]: c.value }), {})
 
 // This exposes the variables to the build. They will be available as `calculated.programme.events.x`
 // (where `x` is the name in the export object)
@@ -44,5 +47,9 @@ export default {
 		all: allEventsByMonth,
 		open: expandToRange(openEventsByMonth,dates),
 	},
+  categories: {
+    all: all_by_category,
+    open: open_by_category,
+  },
 	dates: dates,
 };

data/metadata.dvc
@@ -1,11 +1,11 @@
-md5: e6798f6ada296fcecde7e94dc7e21dde
+md5: eaf98a59ad9ad270a3d0794170386b13
 frozen: true
 deps:
 - path: data/metadata/processed/
   repo:
     url: git@github.com:bradford-2025/open-data-pipelines
     rev: main
-    rev_lock: 56ed7faead9a3bd74cc378fcf52e9974a50bca82
+    rev_lock: 9c515492d31e0458f3eb617fad1db3a2b0b6cdf4
 outs:
 - hash: md5
   path: metadata

data/processed.dvc
@@ -1,11 +1,11 @@
-md5: eaa68cdf7cea539a788202383df47cd4
+md5: be04823ca47f154838c3e58e9aba2b57
 frozen: true
 deps:
 - path: data/processed/
   repo:
     url: git@github.com:bradford-2025/open-data-pipelines
     rev: main
-    rev_lock: 56ed7faead9a3bd74cc378fcf52e9974a50bca82
+    rev_lock: 9c515492d31e0458f3eb617fad1db3a2b0b6cdf4
 outs:
 - hash: md5
   path: processed

data/published.dvc
@@ -1,14 +1,14 @@
-md5: 4e51d8a5f247f244893c959077a87f0b
+md5: 1ccad11e3efcd2fff466c5d16749ad26
 frozen: true
 deps:
 - path: data/published/
   repo:
     url: git@github.com:bradford-2025/open-data-pipelines
     rev: main
-    rev_lock: 56ed7faead9a3bd74cc378fcf52e9974a50bca82
+    rev_lock: 9c515492d31e0458f3eb617fad1db3a2b0b6cdf4
 outs:
-- md5: 931f3a900b312c6038dad859ece7a3be.dir
-  size: 120448
+- md5: 503d479f33441c9e40dc31d6b8777472.dir
+  size: 121849
   nfiles: 10
   hash: md5
   path: published

data/reports.dvc
@@ -1,11 +1,11 @@
-md5: 109afa7138f81b04648918c09bee16fa
+md5: 4be6aa76478c97faec1f539d8f218147
 frozen: true
 deps:
 - path: data/reports/
   repo:
     url: git@github.com:bradford-2025/open-data-pipelines
     rev: main
-    rev_lock: 56ed7faead9a3bd74cc378fcf52e9974a50bca82
+    rev_lock: 9c515492d31e0458f3eb617fad1db3a2b0b6cdf4
 outs:
 - md5: 3348043a4ccf0a2f004d6fe8068ce160.dir
   size: 403765

data/status.json.dvc
@@ -1,10 +1,10 @@
-md5: ea2561b5b6ee8602937cfdb83a33e706
+md5: 39e68062529017dc738dc3fee215753a
 frozen: true
 deps:
 - path: data/metadata/status.json
   repo:
     url: git@github.com:bradford-2025/open-data-pipelines
-    rev_lock: 56ed7faead9a3bd74cc378fcf52e9974a50bca82
+    rev_lock: 9c515492d31e0458f3eb617fad1db3a2b0b6cdf4
 outs:
 - md5: 8e299380f8f7d76fc5bd5f77b88167c6
   size: 271

src/events/index.vto
@@ -12,7 +12,9 @@ tags:
 ---
 
 {{>
-	const { timestamp, events, projects, metrics } = search.data("insights/dashboard");
+	console.warn("%cWARNING:", "background-color: yellow;", 'Pulling data from internal source');
+	// Suffixing these with _d to make it easier to remove them when replacing with open data
+	const { timestamp, events: events_d, projects: projects_d, metrics } = search.data("insights/dashboard");
 }}
 
 <section>
@@ -24,7 +26,7 @@ tags:
 		{{ comp grid.Brick { tag: 'li', classes: 'events block' } }}
 			<h3>{{ metrics.events_in_person.title }}</h3>
 			{{ comp.BigNumber({
-				number: Object.entries(events.by_category).filter(c => ![ 'Exhibition', 'Festival', 'Digital' ].includes(c[0])).reduce((a, c) => a + c[1], 0) || '&mdash;',
+				number: Object.entries(calculated.programme.events.categories.open).filter(c => ![ 'Exhibition', 'Festival', 'Digital' ].includes(c[0])).reduce((a, c) => a + c[1], 0) || '&mdash;',
 				classes: 'big',
 				animate: true,
 				roundingOptions: { maximumFractionDigits: 0 },
@@ -35,7 +37,7 @@ tags:
 		{{ comp grid.Brick { tag: 'li', classes: 'events block' } }}
 			<h3>{{ metrics.events_online.title }}</h3>
 			{{ comp.BigNumber({
-				number: events.by_category['Online'] || '&mdash;',
+				number: calculated.programme.events.categories.open.Digital || '&mdash;',
 				classes: 'big',
 				animate: true,
 				roundingOptions: { maximumFractionDigits: 0 },
@@ -47,12 +49,13 @@ tags:
 	{{ comp grid.Auto { tag: 'ul', classes: 'dashboard' } }}
 		{{> 
 			const festivals = [];
-			for(e in events.by_project){
-				if(events.by_project[e].category === 'Festival'){
-					festivals.push({"title":e,"value":events.by_project[e].count});
+			for(e in events_d.by_project){
+				if(events_d.by_project[e].category === 'Festival'){
+					festivals.push({"title":e,"value":events_d.by_project[e].count});
 				}
 			}
 		}}
+		{{# TODO work out how to get this from somewhere else or remove? #}}
 		{{ comp grid.Brick { tag: 'li', classes: 'events block' } }}
 			<h3>{{ metrics.events_festivals.title }}</h3>
 			{{ comp.BigNumber({
@@ -67,7 +70,7 @@ tags:
 		{{ comp grid.Brick { tag: 'li', classes: 'events block' } }}
 			<h3>{{ metrics.events_festival_events.title }}</h3>
 			{{ comp.BigNumber({
-				number: events.by_category.Festival || 0,
+				number: calculated.programme.events.categories.open.Festival || 0,
 				classes: 'big',
 				animate: true,
 				roundingOptions: { maximumFractionDigits: 0 },
@@ -75,10 +78,11 @@ tags:
 			}) }}
 			<p>Count of {{ comp glossary.Ref { slug: 'festival' } }}Festival events{{ /comp }}.</p>
 		{{ /comp }}
+		{{# TODO work out how to get this from somewhere else or remove? #}}
 		{{ comp grid.Brick { tag: 'li', classes: 'events block' } }}
 			<h3>{{ metrics.events_exhibitions.title }}</h3>
 			{{ comp.BigNumber({
-				number: projects.by_category['Exhibition'] || 0,
+				number: projects_d.by_category['Exhibition'] || 0,
 				classes: 'big',
 				animate: true,
 				roundingOptions: { maximumFractionDigits: 0 },
@@ -89,7 +93,7 @@ tags:
 		{{ comp grid.Brick { tag: 'li', classes: 'events block' } }}
 			<h3>{{ metrics.events_exhibition_days.title }}</h3>
 			{{ comp.BigNumber({
-				number: events.by_category.Exhibition || 0,
+				number: calculated.programme.events.categories.open.Exhibition || 0,
 				classes: 'big',
 				animate: true,
 				roundingOptions: { maximumFractionDigits: 0 },
@@ -100,7 +104,7 @@ tags:
 		{{ comp grid.Brick { tag: 'li', classes: 'events block' } }}
 			<h3>{{ metrics.events_community_led.title }}</h3>
 			{{ comp.BigNumber({
-				number: (events.by_project['Our Patch']?.count || 0) + (events.by_category["Community-led arts event"] || 0),
+				number: (events_d.by_project['Our Patch']?.count || 0) + (events_d.by_category["Community-led arts event"] || 0),
 				classes: 'big',
 				animate: true,
 				roundingOptions: { maximumFractionDigits: 0 },
@@ -111,7 +115,7 @@ tags:
 		{{ comp grid.Brick { tag: 'li', classes: 'events block' } }}
 			<h3>{{ metrics.events_free_activities.title }}</h3>
 			{{ comp.BigNumber({
-				number: events.free || '&mdash;',
+				number: events_d.free || '&mdash;',
 				classes: 'big',
 				animate: true,
 				roundingOptions: { maximumFractionDigits: 0 },

commit b3e1326371ca2dff452c42cf712a2ee4d1a1c3b7

Fix bug with monthly events chart
Seems to have been introduced with f0476aa

src/_data/calculated/programme/events.js
@@ -23,12 +23,11 @@ const allEvents = totals.find((r) =>
 // MONTHLY DATA
 const byMonth = await loadCsv("src/_data/published/programme/by_month.csv");
 const allEventsByMonth = byMonth.filter((r) =>
-  r.aggregation == 'BY_MONTH'
+  r.aggregation == 'BY_MONTH' && r.variable == 'events'
 ).map(cleanGraphData);
 
 const openEventsByMonth = byMonth.filter((r) =>
-  r.aggregation == 'BY_MONTH_BY_EVENT_TYPE' &&
-  r.event_type == "Open"
+  r.aggregation == 'BY_MONTH_BY_EVENT_TYPE' && r.event_type == "Open" && r.variable == 'events'
 ).map(cleanGraphData);
 
 const dates = minMaxDates(allEventsByMonth,'Date');