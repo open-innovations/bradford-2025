180bf1df82cc2b3b45715f04225744c78e7f380c
slowe <299787+slowe@users.noreply.github.com>
2026-03-12 17:07:38 +0000

    Update positive experience on dashboards
    No longer uses the manual figures. Instead it is calculated by summing up positive responses for each month included on a dashboard and dividing by the total responses in that period.

--- src/_data/calculated/survey.js
+++ src/_data/calculated/survey.js
@@ -1,6 +1,7 @@
-import { loadCsv, cleanGraphData, minMaxDates, expandToRange } from "lib/data-helpers.js";
+import { loadCsv, loadYaml, cleanGraphData, minMaxDates, expandToRange } from "lib/data-helpers.js";
 
 const results = await loadCsv('src/_data/published/survey/results.csv');
+const params = await loadYaml('pipelines/params.yaml');
 
 function breakdown(variable){
 
@@ -9,6 +10,30 @@ function breakdown(variable){
 	).map(cleanGraphData).map(x => ({ ...x, percent: x.value*100 }));
 
 	const dates = minMaxDates(byMonth,'Date');
+	
+	const dashboards = {};
+	if(variable == "rated_good_or_excellent"){
+		// Build monthly values
+		const monthly = {};
+		for(let i = 0; i < results.length; i++){
+			if(results[i].aggregation == 'BY_MONTH'){
+				let month = results[i].month;
+				if(!(month in monthly)) monthly[month] = {};
+				monthly[month][results[i].variable] = results[i].value;
+			}
+		}
+		for(let d in params.dashboards){
+			dashboards[d] = 0;
+			let responses = 0;
+			for(let dt in monthly){
+				if(dt >= params.dashboards[d].start && dt <= params.dashboards[d].end){
+					responses += monthly[dt].responses;
+					dashboards[d] += monthly[dt].responses * monthly[dt].rated_good_or_excellent;
+				}
+			}
+			dashboards[d] *= 100/responses;
+		}
+	}
 
 	return {
 
@@ -20,6 +45,8 @@ function breakdown(variable){
 		monthly: {
 			all: expandToRange(byMonth,dates),
 		},
+
+		dashboards: dashboards,
 
 		dates: dates
 
